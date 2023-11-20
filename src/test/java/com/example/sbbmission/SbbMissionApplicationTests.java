c1c35c04c2a0b1468fdcf051270231081da658d2
taehoon <xogns2134@gmail.com>
2023-11-20 14:33:00 +0900

    update test

--- src/test/java/com/example/sbbmission/SbbMissionApplicationTests.java
+++ src/test/java/com/example/sbbmission/SbbMissionApplicationTests.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SbbMissionApplicationTests {
@@ -60,6 +61,15 @@ class SbbMissionApplicationTests {
         List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
         Question q = qList.get(0);
         assertEquals("sbb가 무엇인가요?",q.getSubject());
+    }
+
+    @Test
+    void update() {
+        Optional<Question> oq = this.questionRepository.findById(1);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+        q.setSubject("수정된 제목");
+        this.questionRepository.save(q);
     }
 
 }
