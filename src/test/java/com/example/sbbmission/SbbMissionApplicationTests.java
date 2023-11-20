a1c308f49bc4ef1e0be26c4ca75d579edbe18eea
taehoon <xogns2134@gmail.com>
2023-11-20 15:21:52 +0900

    yml수정+answerAboutQuestion test

--- src/test/java/com/example/sbbmission/SbbMissionApplicationTests.java
+++ src/test/java/com/example/sbbmission/SbbMissionApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.sbbmission;
 
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,6 +16,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class SbbMissionApplicationTests {
     @Autowired
     private QuestionRepository questionRepository;
+
+    @Autowired
+    private AnswerRepository answerRepository;
 
     @Test
     void createQuestion() {
@@ -72,4 +76,37 @@ class SbbMissionApplicationTests {
         assertEquals(1,this.questionRepository.count());
     }
 
+    @Test
+    void createAnswer(){
+        Optional<Question> oq = this.questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+
+        Answer a = new Answer();
+        a.setContent("네 자동으로 생성됩니다.");
+        a.setQuestion(q); //외래키로 사용하는 Question객체
+        a.setCreateDate(LocalDateTime.now());
+        this.answerRepository.save(a);
+    }
+
+    @Test
+    void findAnswer(){
+        Optional<Answer> oa = this.answerRepository.findById(1);
+        assertTrue(oa.isPresent());
+        Answer a = oa.get();
+        assertEquals(2,a.getQuestion().getId());
+    }
+
+    @Test
+    @Transactional
+    void answerAboutQuestion() {
+        Optional<Question> oq = this.questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+
+        List<Answer> answerList = q.getAnswerList();
+
+        assertEquals(1, answerList.size());
+        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+    }
 }
