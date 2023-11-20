020e3fc95222337aa33bc3b90419950cd94fe3b6
taehoon <xogns2134@gmail.com>
2023-11-20 13:09:35 +0900

    2-2-2

--- src/main/java/com/example/sbbmission/MainController.java
+++ src/main/java/com/example/sbbmission/MainController.java
@@ -2,12 +2,14 @@ package com.example.sbbmission;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
     @GetMapping("/index")
-    public void index(){
-        System.out.println("index");
+    @ResponseBody
+    public String index(){
+        return "index";
     }
 }
