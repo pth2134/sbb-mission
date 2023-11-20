c2e42361395d38fd0cd8fab17806c9828e4f0615
taehoon <xogns2134@gmail.com>
2023-11-20 14:16:25 +0900

    2-5 findAll

--- src/test/java/com/example/sbbmission/SbbMissionApplicationTests.java
+++ src/test/java/com/example/sbbmission/SbbMissionApplicationTests.java
@@ -5,13 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SbbMissionApplicationTests {
     @Autowired
     private QuestionRepository questionRepository;
+
     @Test
-    void testJpa() {
+    void createQuestion() {
         Question q1 = new Question();
         q1.setSubject("sbb가 무엇인가요?");
         q1.setContent("sbb에 대해서 알고 싶습니다.");
@@ -21,8 +25,19 @@ class SbbMissionApplicationTests {
         Question q2 = new Question();
         q2.setSubject("스프링부트 모델 질문입니다.");
         q2.setContent("id는 자동으로 생성되나요?");
-        q2.setCreateDate(LocalDateTime.now());;
+        q2.setCreateDate(LocalDateTime.now());
+        ;
         this.questionRepository.save(q2);
     }
+
+    @Test
+    void findAll() {
+        List<Question> all = this.questionRepository.findAll();
+        assertEquals(2,all.size());
+
+        Question q = all.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
+    }
+
 
 }
