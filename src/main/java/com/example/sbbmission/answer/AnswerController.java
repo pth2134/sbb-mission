1d891aa4dd1d6b533a6225834a48df8e5bb8c538
taehoon <xogns2134@gmail.com>
2023-11-23 16:54:35 +0900

    AnswerService,AnswerController,question_detail

---
+++ src/main/java/com/example/sbbmission/answer/AnswerController.java
@@ -0,0 +1,28 @@
+package com.example.sbbmission.answer;
+
+import com.example.sbbmission.question.Question;
+import com.example.sbbmission.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/answer")
+public class AnswerController {
+
+    private final QuestionService questionService;
+    private final AnswerService answerService;
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content){
+        Question question = this.questionService.getQuestion(id);
+        Answer answer = new Answer();
+        answerService.create(question,content);
+        answer.setQuestion(question);
+        return String.format("redirect:/question/detail/%s",id);
+    }
+}
