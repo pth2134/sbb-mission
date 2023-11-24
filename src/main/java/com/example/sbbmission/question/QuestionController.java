c665f5befd364643ff8acbdb66a6064a0789e555
taehoon <xogns2134@gmail.com>
2023-11-24 17:36:38 +0900

    2-15 thymeleaf상태가 안좋음

--- src/main/java/com/example/sbbmission/question/QuestionController.java
+++ src/main/java/com/example/sbbmission/question/QuestionController.java
@@ -1,11 +1,11 @@
 package com.example.sbbmission.question;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,16 +16,31 @@ public class QuestionController {
     private final QuestionService questionService;
 
     @GetMapping("/list")
-    public String list(Model model){
+    public String list(Model model) {
         List<Question> questionList = this.questionService.getList();
-        model.addAttribute("questionList",questionList);
+        model.addAttribute("questionList", questionList);
         return "question_list";
     }
 
     @GetMapping(value = "/detail/{id}")
-    public String detail(Model model, @PathVariable Integer id){
+    public String detail(Model model, @PathVariable Integer id) {
         Question question = this.questionService.getQuestion(id);
-        model.addAttribute("question",question);
+        model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    @GetMapping("/create")
+    public String questionCreate(QuestionForm questionForm) {
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@Valid QuestionForm questionForm
+            , BindingResult bindingResult) {
+        if(bindingResult.hasErrors()){
+            return "question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+        return "redirect:/question/list";
     }
 }
