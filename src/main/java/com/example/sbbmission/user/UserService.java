7264c3680b1c74d3db4243a64119af8e5facac5b
taehoon <xogns2134@gmail.com>
2023-11-28 17:38:34 +0900

    3-6 PasswordEncoder 빈 등록

--- src/main/java/com/example/sbbmission/user/UserService.java
+++ src/main/java/com/example/sbbmission/user/UserService.java
@@ -1,7 +1,7 @@
 package com.example.sbbmission.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,12 +9,11 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
-
+    private final PasswordEncoder passwordEncoder;
     public SiteUser create(String username, String email, String password){
         SiteUser user = new SiteUser();
         user.setUsername(username);
         user.setEmail(email);
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         user.setPassword(passwordEncoder.encode(password));
         this.userRepository.save(user);
         return user;
