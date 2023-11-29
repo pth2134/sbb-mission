f04f30cb8f04f26d7c8c43199901ead6e9527e8c
taehoon <xogns2134@gmail.com>
2023-11-29 16:52:52 +0900

    3-7 authenticationManager Bean

--- src/main/java/com/example/sbbmission/user/UserSecuriyService.java
+++ src/main/java/com/example/sbbmission/user/UserSecuriyService.java
@@ -3,6 +3,7 @@ package com.example.sbbmission.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
