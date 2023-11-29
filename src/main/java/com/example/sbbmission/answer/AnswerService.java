2bf0303365e6f90dfcf19126c3a02e141921fabd
taehoon <xogns2134@gmail.com>
2023-11-29 19:40:54 +0900

    3-10 수정과 삭제

--- src/main/java/com/example/sbbmission/answer/AnswerService.java
+++ src/main/java/com/example/sbbmission/answer/AnswerService.java
@@ -1,11 +1,13 @@
 package com.example.sbbmission.answer;
 
+import com.example.sbbmission.DataNotFoundException;
 import com.example.sbbmission.question.Question;
 import com.example.sbbmission.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +23,25 @@ public class AnswerService {
         answer.setAuthor(author);
         this.answerRepository.save(answer);
         return answer;
+    }
+
+    public Answer getAnswer(Integer id){
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if(answer.isPresent()){
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
     }
 
 }
