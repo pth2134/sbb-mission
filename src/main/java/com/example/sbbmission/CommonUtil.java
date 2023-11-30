9a53d97dfb944c9e924a11a88364eab45e3684f7
taehoon <xogns2134@gmail.com>
2023-11-30 10:11:52 +0900

    3-13 markdown util

--- src/main/java/com/example/sbbmission/CommonUtil.java
+++ src/main/java/com/example/sbbmission/CommonUtil.java
@@ -1,2 +1,16 @@
-package com.example.sbbmission;public class CommonUtil {
+package com.example.sbbmission;
+
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommonUtil {
+    public String markdown(String markdown){
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(markdown);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(document);
+    }
 }
